9359587e37ff9d42ac4d05b4d1c358991959f976
fendijatmiko <jatmikofendi@gmail.com>
2023-03-09 06:46:54 +0700

    need sudo to run this

--- updater.py
+++ updater.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 
-import shutil
+# import shutil
+import os
 import subprocess
 
 # define src, and dst
 src  = "00-welcome"
 dst ="/etc/update-motd.d/00-welcome"
+dir_path = "/etc/update-motd.d/"
 
 # shutil.copyfile(src, dst)
-subprocess.run(["sudo", "cp", src, dst])
+# clean existing
+for file in os.listdir(dir_path):
+    file_path = os.path.join(dir_path, file)
+    os.remove(file_path)
 
-# subprocess.run(["sudo", "cp", "00-welcome", "/etc/update-motd.d/" ])
+# subprocess.run(["sudo", "cp", src, dst])
+
+subprocess.run(["sudo", "cp", "00-welcome", "/etc/update-motd.d/" ])
 subprocess.run(["ls", "-la", "/etc/update-motd.d"])
